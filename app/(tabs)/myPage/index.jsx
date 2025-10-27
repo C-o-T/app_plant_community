f487d86bcedcb96e6a78600b417d6ee48ab0d7a3
duswn7740 <duswn7740@hanmail.net>
2025-10-27 12:35:12 +0900

    회원가입기능 구현

--- app/(tabs)/myPage/index.jsx
+++ app/(tabs)/myPage/index.jsx
@@ -1,55 +1,27 @@
-import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
-import React, { useState, useEffect } from 'react'
+import { StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import Button from '../../../components/common/Button'
+import Button from '@/components/common/Button'
 import { useRouter } from 'expo-router'
-import { Keyboard } from 'react-native'
 import * as SecureStore from 'expo-secure-store'
+import { colors } from '@/constants/colorConstant'
 
 const MyPageScreen = () => {
   const router = useRouter();
-  const [loginInfo, setLoginInfo] = useState(null);
-
-  useEffect(() => {
-    // 로그인 정보 가져오기
-    const getLoginInfo = async () => {
-      const info = await SecureStore.getItemAsync('loginInfo');
-      if (info) {
-        setLoginInfo(JSON.parse(info));
-      }
-    };
-    getLoginInfo();
-  }, []);
 
   //logout 실행시 실행할 함수
   const handleLogout = async () => {
     await SecureStore.deleteItemAsync('loginInfo');
-    setLoginInfo(null);
     router.replace('/auth/login');
   }
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView>
-          <Text>전체메뉴</Text>
-            {
-              loginInfo === null 
-              ?
-              <Button 
-              title = '로그인'
-              onPress={()=>router.push('/auth/login')}
-              />
-              :
-              <Button
-                title='로그아웃'
-                onPress={handleLogout}
-              />
-            }
-            
-            <Button 
-              title = '회원가입'
-              onPress={()=>router.push('/auth/join')}
-            />
+      <SafeAreaView style={styles.container}>
+        <Button
+          title='로그아웃'
+          backgroundColor={colors.SUB1}
+          onPress={handleLogout}
+        />
       </SafeAreaView>
     </TouchableWithoutFeedback>
   )
@@ -57,4 +29,9 @@ const MyPageScreen = () => {
 
 export default MyPageScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor : colors.WHITE
+  }
+})
--- app/auth/login.jsx
+++ app/auth/login.jsx
@@ -1,10 +1,12 @@
-import { Button, Pressable, StyleSheet, Text, TextInput, TouchableWithoutFeedback, View, Keyboard } from 'react-native'
+import { Pressable, StyleSheet, Text, TouchableWithoutFeedback, View, Keyboard } from 'react-native'
 import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import axios from 'axios'
-import { useRouter } from 'expo-router'
+import { router, useRouter } from 'expo-router'
+import Button from '@/components/common/Button'
 import Input from '@/components/common/Input.jsx'
 import * as SecureStore from 'expo-secure-store'
+import { colors } from '../../constants/colorConstant'
 
 const login = () => {
   const route = useRouter();
@@ -16,7 +18,7 @@ const login = () => {
   //로그인 함수
   const handleLogin = async () => {
     // Android Emulator는 10.0.2.2, 실제 디바이스는 192.168.30.97
-    const API_URL = 'http://10.0.2.2:5000/members/login';
+    const API_URL = 'http://10.0.2.2:8080/members/login';
 
     try {
       const res = await axios.get(API_URL, {
@@ -48,25 +50,32 @@ const login = () => {
 
 
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={styles.container}>
       <SafeAreaView>
 
-        <TextInput
+        <Input
           placeholder="아이디"
           value={loginInfo.memId}
           onChangeText={(text) => setLoginInfo({...loginInfo, memId: text})}
         />
-        <TextInput
+        <Input
           placeholder="비밀번호"
           value={loginInfo.memPw}
           onChangeText={(text) => setLoginInfo({...loginInfo, memPw: text})}
           secureTextEntry
         />
-        <Pressable
+        <Button
           onPress={handleLogin}
-        ><Text>로그인</Text></Pressable>
+          title='로그인'
+        >
+        </Button>
+        <Button
+          onPress={()=>router.push('/auth/join')}
+          title='회원가입'
+          backgroundColor={colors.SUB1}
+        >
+        </Button>
 
-        <Text>join</Text>
       </SafeAreaView>
     </TouchableWithoutFeedback>
   )
@@ -74,4 +83,9 @@ const login = () => {
 
 export default login
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container : {
+    flex : 1,
+    backgroundColor : colors.WHITE
+  }
+})
