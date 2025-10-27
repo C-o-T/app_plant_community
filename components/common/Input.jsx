6480b56bb6cd9be45fa4b075a22173834fcbe92e
park1541 <a059771@gmail.com>
2025-10-27 17:20:42 +0900

    커뮤니티 게시글 목록조회, 글쓰기화면, editor

--- components/common/Input.jsx
+++ components/common/Input.jsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
 
 const Input = () => {
   return (
     <View>
-      <Text>Input</Text>
+      <TextInput />
     </View>
   )
 }
